2483a452ac18f4905467e66ab2c272969f542050
chidiewenike <chidiewenike@gmail.com>
2018-09-23 20:53:38 -0700

    Lab#0 09/23/18 20:52

--- planets.py
+++ planets.py
@@ -1,7 +1,8 @@
+# function body for calculating the weights
 def weight_on_planets():
-   # write your code here
-   
-   
+    #
+    weightVal = float(input("What do you weigh on earth? "))
+    print(f'\nOn Mars you would weigh {0.38*weightVal} pounds.\nOn Jupiter you would weigh {2.34*weightVal} pounds.')
    
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
